a2712af6068d5d8f62c0db5a7373d153636c9e0e
Alexandr611 <smalinovsky611@gmail.com>
2020-10-15 01:20:04 +0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
+const RESET = 'reset';
+
+const initialState = {
+  count: 0,
+  score: 0
+};
+
+const reducer = (state, payload) => {
+  console.log('state: ', state);
+  console.log('payload: ', payload);
+  switch(payload.type) {
+    case INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case RESET:
+      return { ...state ,count: 0 };
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState({
+  //   counter: 0
+  // });
   const [counter1, setCounter1] = useState(0);
+  const [counter, dispatch] = useReducer(reducer,  initialState); // under the hood -> useState
 
   useEffect(() => {
     console.log('enter useEffect');
@@ -12,7 +39,7 @@ function App() {
       console.log('setInteval');
     }, 1000 * 2)
 
-    
+    clearInterval(interval);
     
     const cleanUp = () => {
       clearInterval(interval);
@@ -20,25 +47,29 @@ function App() {
     return cleanUp;
   }, [counter])
   
-  const handleClick = (type) => {
-    if (type === 'increment') {
-      setCounter(() => counter + 1);
-    } else if (type === 'decrement') {
-      setCounter(() => counter - 1);
-    }
-  }
+  // const handleClick = (type) => {
+  //   if (type === 'increment') {
+  //     setCounter((prevCounter) => ({ counter: prevCounter.counter + 1 }));
+  //   } else if (type === 'decrement') {
+  //     setCounter((prevCounter) => ({ counter: prevCounter.counter - 1 }));
+  //   }
+  // }
 
   return (
     <div className="App">
-      <button onClick={() => handleClick('increment')}>
+      <button onClick={() => dispatch({ type: INCREMENT })}>
         click to increment value
       </button>
-      
-      <button onClick={() => handleClick('decrement')}>
+      <br/>
+      <button onClick={() => dispatch({ type: DECREMENT })}>
         click to decrement value
       </button> 
-
-      value is {counter}
+      <br/>
+      <button onClick={() => dispatch({ type: RESET })}>
+        click to reset value
+      </button> 
+      <br/>
+      value is {counter.count}
     </div>
   );
 }
