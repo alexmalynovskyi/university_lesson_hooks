482eca3286a9916eab8c1b4b2e67acfaf649a227
Alexandr611 <smalinovsky611@gmail.com>
2020-10-15 00:54:28 +0300

    useState example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  
+  const handleClick = (type) => {
+    if (type === 'increment') {
+      setCounter(() => counter + 1);
+    } else if (type === 'decrement') {
+      setCounter(() => counter - 1);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => handleClick('increment')}>
+        click to increment value
+      </button> 
+
+      <button onClick={() => handleClick('decrement')}>
+        click to decrement value
+      </button> 
+
+      value is {counter}
     </div>
   );
 }
